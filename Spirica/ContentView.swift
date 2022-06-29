d2d979052581083f381f5856213551d47503389c
shilrey <shirleychung1980@gmail.com>
2022-06-29 19:40:35 +0800

    Initial Commit

---
+++ Spirica/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spirica
+//
+//  Created by shirley on 2022/6/29.
+//
+
+import SwiftUI
+import OctopusKit
+
+struct ContentView: View {
+    var body: some View {
+        OKQuickStartView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spirica/SpiricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiricaApp.swift
+//  Spirica
+//
+//  Created by shirley on 2022/6/29.
+//
+
+import SwiftUI
+
+@main
+struct SpiricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
